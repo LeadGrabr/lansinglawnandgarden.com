6b972ab6fc02d0e02964d249731ab7a4df092472
Benjamin Tatum <bentatum@me.com>
2016-04-14 11:56:01 -0400

    more on testimonials

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,10 +1,11 @@
-import { default as React, PropTypes } from 'react'
+import { default as React, Component, PropTypes } from 'react'
 import { Flex, Box } from 'reflexbox'
 import { Base, Heading, Text } from 'rebass'
 import { default as headShotOne } from './testimonial-headshot-1.jpg'
 import { default as headShotTwo } from './testimonial-headshot-2.jpg'
 import { default as headShotThree } from './testimonial-headshot-3.jpg'
 import { default as headShotFour } from './testimonial-headshot-4.jpg'
+import { connect } from 'react-redux'
 
 const imgSize = 100
 
@@ -15,12 +16,17 @@ const sampleCopy = `
     sed, ultricies sapien.
 `
 
-const Testimonial = ({ img, name, text }) =>
+const Testimonial = ({ img, name, text, width }) =>
     <Flex
+        align="center"
+        column
         mb={2}
-        style={{ width: '50%' }}
+        style={{ width }}
     >
-        <Box px={2}>
+        <Box
+            mb={2}
+            px={2}
+        >
             <Base
                 circle
                 style={{
@@ -32,10 +38,19 @@ const Testimonial = ({ img, name, text }) =>
             />
         </Box>
         <Box px={2}>
-            <Heading level={4}>
+            <Heading
+                level={4}
+                style={{
+                    textAlign: 'center'
+                }}
+            >
                 {name}
             </Heading>
-            <Text>
+            <Text
+                style={{
+                    textAlign: 'center'
+                }}
+            >
                 {text}
             </Text>
         </Box>
@@ -44,31 +59,57 @@ const Testimonial = ({ img, name, text }) =>
 Testimonial.propTypes = {
     img: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    width: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string
+    ]).isRequired
 }
 
-const Testimonials = () => 
-    <Flex wrap>
-        <Testimonial
-            img={headShotOne}
-            name="Joe Schmoe"
-            text={sampleCopy}
-        />
-        <Testimonial
-            img={headShotTwo}
-            name="Foo Bario"
-            text={sampleCopy}
-        />
-        <Testimonial
-            img={headShotThree}
-            name="Katrina Green"
-            text={sampleCopy}
-        />
-        <Testimonial
-            img={headShotFour}
-            name="Josephina Turducken"
-            text={sampleCopy}
-        />
-    </Flex>
+@connect(({ app: { width } }) => ({ width }))
 
-export default Testimonials
+export default class Testimonials extends Component {
+
+    static propTypes = {
+        width: PropTypes.number.isRequired
+    };
+
+    static contextTypes = {
+        breakpoints: PropTypes.object.isRequired
+    };
+
+    render() {
+        const { width } = this.props
+        const { breakpoints } = this.context
+        const { medium } = breakpoints
+        const testimonialWidth = width > medium ? '50%' : '100%'
+        return (
+            <Flex wrap>
+                <Testimonial
+                    img={headShotOne}
+                    name="Joe Schmoe"
+                    text={sampleCopy}
+                    width={testimonialWidth}
+                />
+                <Testimonial
+                    img={headShotTwo}
+                    name="Foo Bario"
+                    text={sampleCopy}
+                    width={testimonialWidth}
+                />
+                <Testimonial
+                    img={headShotThree}
+                    name="Katrina Green"
+                    text={sampleCopy}
+                    width={testimonialWidth}
+                />
+                <Testimonial
+                    img={headShotFour}
+                    name="Josephina Turducken"
+                    text={sampleCopy}
+                    width={testimonialWidth}
+                />
+            </Flex>
+        )
+    }
+}
