60e6304d5752d5fa9819f597fe531e5216ca4ba0
Benjamin Tatum <bentatum@me.com>
2016-04-20 13:28:20 -0400

    testing bad commit message

--- src/components/Home/Badges.js
+++ src/components/Home/Badges.js
@@ -1,61 +1,9 @@
-import { default as React, Component, createElement, PropTypes } from 'react'
-import { Flex } from 'reflexbox'
+import { default as React, Component, PropTypes } from 'react'
 import { default as Accessible } from 'react-icons/lib/md/accessible'
-import { Base, Heading } from 'rebass'
 import { default as Leaf } from 'react-icons/lib/fa/leaf'
 import { default as MagicWand } from 'react-icons/lib/fa/magic'
 import { connect } from 'react-redux'
-
-const badgeSize = 280
-
-const Badge = ({ heading, icon, iconSize, width }, { rebass: { colors } }) =>
-    <Base
-        circle
-        my={2}
-        p={3}
-        style={{
-            alignItems: 'center',
-            backgroundColor: colors.green,
-            display: 'flex',
-            color: colors.white,
-            height: badgeSize,
-            width: badgeSize
-        }}
-    >
-        <Base
-            style={{
-                margin: 'auto',
-                maxWidth: 200,
-                textAlign: 'center'
-            }}
-        >
-            <Heading
-                level={3}
-                mb={2}
-            >
-                {heading}
-            </Heading>
-            <Flex justify="center">
-                {createElement(icon, { size: iconSize })}
-            </Flex>
-        </Base>
-    </Base>
-
-Badge.defaultProps = {
-    iconSize: 80,
-}
-
-Badge.propTypes = {
-    heading: PropTypes.string.isRequired,
-    icon: PropTypes.func.isRequired,
-    iconSize: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired
-}
-
-Badge.contextTypes = {
-    breakpoints: PropTypes.object.isRequired,
-    rebass: PropTypes.object.isRequired
-}
+import { default as BrandBadges } from '@leadgrabr/brand-badges'
 
 @connect(({ app: { width } }) => ({ width }))
 
@@ -68,26 +16,25 @@ export default class Badges extends Component {
 
     render() {
         return (
-            <Flex
-                justify="space-around"
-                wrap
-            >
-                <Badge
-                    {...this.props}
-                    heading="Discounts for seniors and disabled"
-                    icon={Accessible}
-                />
-                <Badge
-                    {...this.props}
-                    heading="100% Green"
-                    icon={Leaf}
-                />
-                <Badge
-                    {...this.props}
-                    heading="You'll love the results"
-                    icon={MagicWand}
-                />
-            </Flex>
+            <BrandBadges
+                badges={[
+                    {
+                        ...this.props,
+                        icon: Accessible,
+                        heading: 'Discounts for seniors and disabled'
+                    },
+                    {
+                        ...this.props,
+                        icon: Leaf,
+                        heading: '100% Green'
+                    },
+                    {
+                        ...this.props,
+                        icon: MagicWand,
+                        heading: 'You\'ll love the results'
+                    }
+                ]}
+            />
         )
     }
 }
